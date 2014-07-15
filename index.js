3253e7f3e476382893e854a6e7a4f7fa1fcb589a
Adrián Escoms <adri@Adrians-MacPro.local>
2014-07-16 00:27:01 +0200

    Update response in index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express')
+var fs = require('fs');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	
+	var buff = new Buffer(fs.readFileSync('index.html'));
+  	response.send(buff.toString());
 })
 
 app.listen(app.get('port'), function() {
